a14b229f319262e9d8fa552494863ae472fa2f98
Shawn <sspringstead@edgeimaging.ca>
2019-02-01 12:47:14 -0500

    throttle cardview
    This will prevent image calls when just moving the cursor around

--- src/client/js/actions/BuilderActions.js
+++ src/client/js/actions/BuilderActions.js
@@ -1,11 +1,12 @@
 import { dispatch } from '../dispatcher';
 import { BuilderActions as AT } from '../constants/Actions';
+import { throttle } from 'throttle-debounce';
 
 export const receiveTestData = (data) => dispatch({ type: AT.TEST_RECEIVE, data });
 export const receiveSerieses = (data) => dispatch({ type: AT.SERIESES_RECEIVE, data });
 export const receiveSeries = (data) => dispatch({ type: AT.SERIES_RECEIVE, data });
 
-export const selectCard = (data) => dispatch({ type: AT.SELECT_CARD, data });
+export const selectCard = throttle( 500, (data) => dispatch({ type: AT.SELECT_CARD, data }) );
 export const filterBuilder = (data) => dispatch({ type: AT.FILTER_BUILDER, data });
 
 /*Deck actions*/
