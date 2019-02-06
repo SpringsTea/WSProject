1e72fed37d43a302162c979d9e2ed18f7640ba9a
Shawn Springstead <shawn.springstead@gmail.com>
2019-02-06 11:31:08 -0500

    Allow mongo auth in production

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,20 @@
 require('@babel/register')({
   
 })
+
+var config = require('./config/mongo.js')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', {
+var mongooseOptions = {
 	useNewUrlParser: true
-});
+}
+
+if( config.AUTH === true ){
+	mongooseOptions.user = config.APP_USERNAME;
+	mongooseOptions.pass = config.APP_PASSWORD;
+}
+
+
+mongoose.connect(`mongodb://127.0.0.1:27017/wsdata?authSource=admin`, mongooseOptions);
 
 module.exports = require('./server.js')
