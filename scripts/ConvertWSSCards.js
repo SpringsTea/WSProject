6dfd87dccb9aeab2dc54ae3fe4346c0fd087c7d4
Shawn Springstead <shawn.springstead@gmail.com>
2019-02-12 15:55:04 -0500

    Changes for set importing

--- scripts/ConvertWSSCards.js
+++ scripts/ConvertWSSCards.js
@@ -1,45 +1,58 @@
 const { readdirSync, readFileSync, writeFile, statSync } = require('fs')
-const { join } = require('path')
-var WSS_PATH = process.env.WSS_PATH || './';
+const { join, extname } = require('path')
+var WSS_PATH = process.env.WSS_PATH || './Cards/';
+var SETDATA_PATH = './SetData/';
 
-var WSS_SETS = readdirSync(WSS_PATH).filter(f => statSync(join(WSS_PATH, f)).isDirectory())
+var WSS_SERIES = readdirSync(WSS_PATH).filter(f => statSync(join(WSS_PATH, f)).isDirectory())
 
-WSS_SETS.forEach( (wss_set) => {
+//add series as env var to only convert that set
+//SERIES=AOT node ConverWSSCards.js
+if( process.env.SERIES ){
+	WSS_SERIES = WSS_SERIES.filter( ( set ) => set === process.env.SERIES )
+}
 
-	var wss_files = readdirSync(wss_set);
+//SERIES IE. AOT LLS etc
+WSS_SERIES.forEach( (wss_series) => {
 
+	var WSS_RELEASES = readdirSync(WSS_PATH+wss_series).filter(f => statSync(join(WSS_PATH+wss_series, f)).isDirectory());
 
-	let set = [];//Put all converted cards into this new set
-	wss_files.forEach( (wss_file) => {
-		let wss_content = readFileSync(`${WSS_PATH}${wss_set}/${wss_file}`, { encoding: 'utf8'});
-		let wss_card = JSON.parse(wss_content);
+	WSS_RELEASES.forEach( ( wss_release ) =>{
+		var wss_files = readdirSync(`${WSS_PATH}${wss_series}/${wss_release}`);
 
-		let card = {
-			sid: wss_card.id,
-			name: cleanText(wss_card.name),
-			set: wss_card.set,
-			side: wss_card.side,
-			release: wss_card.release,
-			cardtype: wss_card.cardType,
-			colour: wss_card.colour,
-			level: parseInt(wss_card.level),
-			cost: parseInt(wss_card.cost),
-			power: parseInt(wss_card.power),
-			soul: parseInt(wss_card.soul),
-			rarity: wss_card.rarity,
-			attributes: wss_card.specialAttrib || [],
-			trigger: wss_card.trigger || [],
-			ability: []
-		}
+		let set = [];//Put all converted cards into this new set
+		wss_files.filter(( file ) => extname(file) === '.json').forEach( (wss_file) => {
 
-		wss_card.ability && wss_card.ability.forEach( (text, i) => { 
-			card.ability.push(cleanText(text));
+			let wss_content = readFileSync(`${WSS_PATH}${wss_series}/${wss_release}/${wss_file}`, { encoding: 'utf8'});
+
+			let wss_card = JSON.parse(wss_content);
+
+			let card = {
+				sid: wss_card.id,
+				name: cleanText(wss_card.name),
+				set: wss_card.set,
+				side: wss_card.side,
+				release: wss_card.release,
+				cardtype: wss_card.cardType,
+				colour: wss_card.colour,
+				level: parseInt(wss_card.level),
+				cost: parseInt(wss_card.cost),
+				power: parseInt(wss_card.power),
+				soul: parseInt(wss_card.soul),
+				rarity: wss_card.rarity,
+				attributes: wss_card.specialAttrib || [],
+				trigger: wss_card.trigger || [],
+				ability: []
+			}
+
+			wss_card.ability && wss_card.ability.forEach( (text, i) => { 
+				card.ability.push(cleanText(text));
+			})
+
+			set.push(card);
 		})
 
-		set.push(card);
+		writeFile(SETDATA_PATH + wss_release+'.json', JSON.stringify(set), 'utf8', (res) => console.log(res) );
 	})
-
-	writeFile(wss_set+'.json', JSON.stringify(set), 'utf8', (res) => console.log(res) );
 
 })
 
