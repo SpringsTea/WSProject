a8a615d826e72c375b032b7a0929d3bd7327205c
Kevin MS <kevin.mansang@mhcomm.fr>
2019-02-22 17:25:52 +0100

    no-unreachable

--- src/client/js/utils/cardshorthands.js
+++ src/client/js/utils/cardshorthands.js
@@ -2,13 +2,10 @@ export const calculateShortHand = (card) => {
   switch (card.cardtype) {
     case 'CX':
       return 'Cx';
-      break;
     case 'CH':
       return `${card.level}/${card.cost} C`;
-      break;
     case 'EV':
       return `${card.level}/${card.cost} E`;
-      break;
     default:
       return '?';
   }
