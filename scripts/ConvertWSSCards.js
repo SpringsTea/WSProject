71a9d094c21498ff3eb2955ea547054c724d88b3
Kevin MS <kevin.mansang@mhcomm.fr>
2019-02-22 17:29:00 +0100

    Use camelcase

--- scripts/ConvertWSSCards.js
+++ scripts/ConvertWSSCards.js
@@ -12,44 +12,44 @@ if ( process.env.SERIES ) {
 }
 
 // SERIES IE. AOT LLS etc
-WSS_SERIES.forEach( (wss_series) => {
-  const WSS_RELEASES = readdirSync(WSS_PATH+wss_series).filter((f) => statSync(join(WSS_PATH+wss_series, f)).isDirectory());
+WSS_SERIES.forEach( (wssSeries) => {
+  const WSS_RELEASES = readdirSync(WSS_PATH+wssSeries).filter((f) => statSync(join(WSS_PATH+wssSeries, f)).isDirectory());
 
-  WSS_RELEASES.forEach( ( wss_release ) =>{
-    const wss_files = readdirSync(`${WSS_PATH}${wss_series}/${wss_release}`);
+  WSS_RELEASES.forEach( ( wssRelease ) =>{
+    const wssFiles = readdirSync(`${WSS_PATH}${wssSeries}/${wssRelease}`);
 
     const set = [];// Put all converted cards into this new set
-    wss_files.filter(( file ) => extname(file) === '.json').forEach( (wss_file) => {
-      const wss_content = readFileSync(`${WSS_PATH}${wss_series}/${wss_release}/${wss_file}`, {encoding: 'utf8'});
+    wssFiles.filter(( file ) => extname(file) === '.json').forEach( (wssFile) => {
+      const wssContent = readFileSync(`${WSS_PATH}${wssSeries}/${wssRelease}/${wssFile}`, {encoding: 'utf8'});
 
-      const wss_card = JSON.parse(wss_content);
+      const wssCard = JSON.parse(wssContent);
 
       const card = {
-        sid: wss_card.id,
-        name: cleanText(wss_card.name),
-        set: wss_card.set,
-        side: wss_card.side,
-        release: wss_card.release,
-        cardtype: wss_card.cardType,
-        colour: wss_card.colour,
-        level: parseInt(wss_card.level),
-        cost: parseInt(wss_card.cost),
-        power: parseInt(wss_card.power),
-        soul: parseInt(wss_card.soul),
-        rarity: wss_card.rarity,
-        attributes: wss_card.specialAttrib || [],
-        trigger: wss_card.trigger || [],
+        sid: wssCard.id,
+        name: cleanText(wssCard.name),
+        set: wssCard.set,
+        side: wssCard.side,
+        release: wssCard.release,
+        cardtype: wssCard.cardType,
+        colour: wssCard.colour,
+        level: parseInt(wssCard.level),
+        cost: parseInt(wssCard.cost),
+        power: parseInt(wssCard.power),
+        soul: parseInt(wssCard.soul),
+        rarity: wssCard.rarity,
+        attributes: wssCard.specialAttrib || [],
+        trigger: wssCard.trigger || [],
         ability: [],
       };
 
-      wss_card.ability && wss_card.ability.forEach( (text, i) => {
+      wssCard.ability && wssCard.ability.forEach( (text, i) => {
         card.ability.push(cleanText(text));
       });
 
       set.push(card);
     });
 
-    writeFile(SETDATA_PATH + wss_release+'.json', JSON.stringify(set), 'utf8', (res) => console.log(res) );
+    writeFile(SETDATA_PATH + wssRelease+'.json', JSON.stringify(set), 'utf8', (res) => console.log(res) );
   });
 });
 
