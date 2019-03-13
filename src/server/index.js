4846ffe4006f6696b7826fce97d18e67c60dc239
Shawn Springstead <shawn.springstead@gmail.com>
2019-03-13 18:44:13 -0400

    Fix connection settings.

--- src/server/index.js
+++ src/server/index.js
@@ -5,20 +5,20 @@ require('@babel/register')({
 //access environmental variables
 require('dotenv').config();
 
-//var config = require('./config/mongo.js');
+var config = require('./config/mongo.js');
 const mongoose = require('mongoose');
 
-/*var mongooseOptions = {
+var mongooseOptions = {
 	useNewUrlParser: true
-}*/
+}
 
 mongoose.set('useCreateIndex', true);
 
-/*if( config.AUTH === true ){
+if( config.AUTH === true ){
 	mongooseOptions.user = config.APP_USERNAME;
 	mongooseOptions.pass = config.APP_PASSWORD;
-}*/
+}
 
-mongoose.connect(`mongodb://localhost:27017/encoreTest`);
+mongoose.connect(process.env.CONNECTION || `mongodb://127.0.0.1:27017/wsdata?authSource=admin`, mongooseOptions);
 
 module.exports = require('./server.js')
