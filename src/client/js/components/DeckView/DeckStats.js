6b634a5d5304c1b991bf1f2a5bfd32aadd214751
Shawn Springstead <shawn.springstead@gmail.com>
2019-03-14 20:59:04 -0400

    Added labels to level pie and misc improvments

--- src/client/js/components/DeckView/DeckStats.js
+++ src/client/js/components/DeckView/DeckStats.js
@@ -1,39 +1,46 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
-import { ResponsiveContainer, PieChart, Pie, Tooltip, Cell } from 'recharts';
+import { PieChart, Pie, Tooltip, Cell } from 'recharts';
 
 class DeckStats extends Component {
-  sumCardQuantity(cards, type) {
-    if (!cards) return [];
+  sumCardQuantity(cards = [], type) {
     return cards.filter((card) => card.cardtype === type).length;
   }
 
-  sumCardColour(cards, colour) {
-    if (!cards) return [];
+  sumCardColour(cards = [], colour) {
     return cards.filter((card) => card.colour === colour).length;
   }
 
-  countCardLevel(cards, level) {
-    if (!cards) return [];
+  countCardLevel(cards = [], level) {
     return cards.filter((card) => card.level === level && card.cardtype !== 'CX').length;
   }
 
-  countCharacterLevel(cards, level) {
-    if (!cards) return [];
-    return cards.filter((card) => card.level === level && card.cardtype !== 'CX' && card.cardtype !== 'EV').length;
-  }
+  renderLevelLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, name, short }) => {
+    const { cards } = this.props;
+    const RADIAN = Math.PI / 180; 
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+    if(percent > 0.05){
+      return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'}  dominantBaseline="central">
+          {short}
+        </text>
+      );
+    }
+    return '';
+  };
 
   render() {
-    const { sumCardQuantity, countCardLevel, countCharacterLevel, sumCardColour } = this;
+    const { renderLevelLabel, sumCardQuantity, countCardLevel, sumCardColour } = this;
     const { cards, deck } = this.props;
 
     const quantitydata = [
-      { "name": 'Level 0', "value": countCharacterLevel(cards, 0) },
-      { "name": 'Level 1', "value": countCharacterLevel(cards, 1) },
-      { "name": 'Level 2', "value": countCharacterLevel(cards, 2) },
-      { "name": 'Level 3', "value": countCharacterLevel(cards, 3) },
-      { "name": 'Event', "value": sumCardQuantity(cards, 'EV') },
-      { "name": 'Climax', "value": sumCardQuantity(cards, 'CX') }
+      { "name": 'Level 0', "short": "0", "value": countCardLevel(cards, 0) },
+      { "name": 'Level 1', "short": "1", "value": countCardLevel(cards, 1) },
+      { "name": 'Level 2', "short": "2", "value": countCardLevel(cards, 2) },
+      { "name": 'Level 3', "short": "3", "value": countCardLevel(cards, 3) },
+      { "name": 'Climax', "short": "CX", "value": sumCardQuantity(cards, 'CX') }
     ];
 
     const typedata = [
@@ -52,53 +59,51 @@ class DeckStats extends Component {
     return (
       <Row gutter={8}>
         <h2>Deck Statistics</h2>
-        <Col span={8}>
+        <Col lg={8}>
           <div>
             <h4>Breakdown by Card Level and Type</h4>
-            <ResponsiveContainer width={350} height={350}>
-              <PieChart>
-                <Pie
-                  data={quantitydata}
-                  dataKey="value"
-                  labelLine={false}
-                  fill="#8884d8"
-                  outerRadius='75%'
-                />
-                <Pie
-                  data={typedata}
-                  dataKey="value"
-                  labelLine={true}
-                  fill="#82ca9d"
-                  innerRadius='65%'
-                  outerRadius='80%'
-                  label={true}
-                />
-                <Tooltip />
-              </PieChart>
-            </ResponsiveContainer>
+            <PieChart width={250} height={250}>
+              <Pie
+                isAnimationActive={false}
+                data={quantitydata}
+                dataKey="value"
+                labelLine={false}
+                label={renderLevelLabel}
+                fill="#8884d8"
+                outerRadius='75%'
+              />
+              <Pie
+                isAnimationActive={false}
+                data={typedata}
+                dataKey="value"
+                labelLine={true}
+                fill="#82ca9d"
+                innerRadius='65%'
+                outerRadius='80%'
+              />
+              <Tooltip />
+            </PieChart>
           </div>
         </Col>
-        <Col span={8}>
+        <Col lg={8}>
           <div>
             <h4>Breakdown by Card Color</h4>
-            <ResponsiveContainer width={350} height={350}>
-              <PieChart>
-                <Pie
-                  data={colourdata}
-                  dataKey="value"
-                  labelLine={false}
-                  fill="#8884d8"
-                  outerRadius='80%'
-                  label={true}
-                  labelLine={true}
-                >
-                  {
-                    colourdata.map((entry, index) => <Cell key={`cell-${index}`} fill={colourdata[index].color} />)
-                  }
-                </Pie>
-                <Tooltip />
-              </PieChart>
-            </ResponsiveContainer>
+            <PieChart width={250} height={250}>
+              <Pie
+                isAnimationActive={false}
+                data={colourdata}
+                dataKey="value"
+                labelLine={false}
+                fill="#8884d8"
+                outerRadius='80%'
+                labelLine={false}
+              >
+                {
+                  colourdata.map((entry, index) => <Cell key={`cell-${index}`} fill={colourdata[index].color} />)
+                }
+              </Pie>
+              <Tooltip />
+            </PieChart>
           </div>
         </Col>
       </Row>
