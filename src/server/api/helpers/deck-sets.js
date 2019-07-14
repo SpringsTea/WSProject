8b1f9f8e7807c2b5b14b6e1436f0baf929488016
Shawn Springstead <shawn.springstead@gmail.com>
2019-07-14 18:40:08 -0400

    Deck save series now uses card.series instead of searching side/release

--- src/server/api/helpers/deck-sets.js
+++ src/server/api/helpers/deck-sets.js
@@ -3,21 +3,16 @@ import Series from '../models/series'
 
 module.exports = async(deck, carddata) => {
 	let deckSets = new Set();
-	let setdata = new Set();
 	
 	for (let cardID of deck.cards) {
 		//get card data from ID
-        let card = carddata.find( c => c._id == cardID );
+                let card = carddata.find( c => c._id == cardID );
 
-        if( card ){
-        	let setparts = `${card.side}${card.release}/${card.lang}`
-        	if( !setdata.has(setparts) ){
-        		setdata.add(setparts)
-
-        		let series = await Series.findOne({side: card.side, release: card.release, lang: card.lang}).exec();
-        		deckSets.add(series._id);
-        	}
-        }
+                if( card ){
+                	if( !deckSets.has(card.series) ){
+                                deckSets.add(card.series);    		
+                	}
+                }
        
 	}
 
