097c83e807c58401f69ecf93b79c90543779138a
Shawn Springstead <shawn.springstead@gmail.com>
2019-07-14 21:01:18 -0400

    Fix sets being added multiple times bug

--- src/server/api/helpers/deck-sets.js
+++ src/server/api/helpers/deck-sets.js
@@ -3,18 +3,22 @@ import Series from '../models/series'
 
 module.exports = async(deck, carddata) => {
 	let deckSets = new Set();
+	let setStrings = new Set();
 	
 	for (let cardID of deck.cards) {
 		//get card data from ID
-                let card = carddata.find( c => c._id == cardID );
+        let card = carddata.find( c => c._id == cardID );
 
-                if( card ){
-                	if( !deckSets.has(card.series) ){
-                                deckSets.add(card.series);    		
-                	}
-                }
-       
+        if( card ){
+        	if( !setStrings.has(card.series.toString()) ){
+        		setStrings.add(card.series.toString());//You cant compare objects in sets, so I store and check the stringified objects aswell
+                deckSets.add(card.series);    		
+        	}
+        }
+
 	}
+
+	console.log(deckSets);
 
 	return Array.from(deckSets);
 }
