cb573148db70f4f1e85cecc12bedaf5ac25ff691
Shawn Springstead <shawn.springstead@gmail.com>
2019-09-08 14:26:36 -0400

    Neostandard sets stored in db

---
+++ src/server/api/models/neoset.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+const neosetSchema = mongoose.Schema({
+	name: { type: String },
+	setcodes: [{ type: String }],
+	enabled: { type: Boolean, default: true },
+	hash: { type: ObjectId }
+}, { collection: 'neosets' })
+
+module.exports = mongoose.model('neoSet', neosetSchema);
