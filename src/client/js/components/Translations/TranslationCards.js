e9dc14a490d26e003713da8022bb20bb8d47c8c4
Shawn Springstead <shawn.springstead@gmail.com>
2019-11-09 15:48:34 -0500

    Add Pagination

--- src/client/js/components/Translations/TranslationCards.js
+++ src/client/js/components/Translations/TranslationCards.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Pagination } from 'antd';
 
 import TranslationCard from './TranslationCard';
 
 class TranslationCards extends Component {
 
+	state={
+		page: 1,
+		pagesize: 8
+	}
+
+	handlePagination = (page) =>{
+		this.setState({page:page})
+	}
+
 	render(){
+		const { handlePagination } = this;
 		const { cards } = this.props;
+		const { page, pagesize } = this.state;
 		return(
 			<div className="container-translationcards">
 				<Row gutter={16}>
-					{ cards.map( (card, i) => 
+					{ cards.slice( (pagesize * page) - pagesize , pagesize * page ).map( (card, i) => 
 						<Col span={12} key={i}>
 							<TranslationCard card={card} />
 						</Col>
 					) }
 				</Row>
+				{
+					cards.length > 0 &&
+					<Pagination defaultCurrent={1} total={cards.length} onChange={handlePagination} />
+				}				
 			</div>
 		)
 	}
