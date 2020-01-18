0fab2affb42e7ac8772c70bebb5a7ff27a749586
Shawn Springstead <shawn.springstead@gmail.com>
2020-01-18 14:17:55 -0500

    Add to form warning

--- src/client/js/constants/copy/FormDownload.js
+++ src/client/js/constants/copy/FormDownload.js
@@ -7,4 +7,9 @@ export const DecklistWarning = () =>
 	  and is not liable for any repercussions that are due to an incorrect registration sheet.
 	</p>
 	<p><strong>Double check your deck list!</strong></p>
+	<p style={{color: 'red'}}>
+	  	Due to technical limitations, Non-English text will not appear anywhere in pdf exports.
+	  	Card names can be entered into the form after export using Adobe Acrobat Reader.
+	  	We apologize for the inconvenience.
+    </p>
 </span>
