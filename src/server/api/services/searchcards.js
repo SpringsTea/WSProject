3a95e17a622243fe7251c6fc42c84f43a0fef5b9
Shawn Springstead <shawn.springstead@gmail.com>
2022-07-29 16:42:22 -0400

    Basic card searching algorithm, will need further testing

--- src/server/api/services/searchcards.js
+++ src/server/api/services/searchcards.js
@@ -9,6 +9,9 @@ import Card from '../models/card'
 const mongoose = require('mongoose')
 
 var ObjectId = mongoose.Types.ObjectId;
+const quoteexperession = new RegExp(/(["'])(?:(?=(\\?))\2.)*?\1/)
+const cardtypes = ['CX', 'CH', 'EV'];
+const cardcolours = [ "YELLOW", "GREEN", "RED", "BLUE", "PURPLE" ];
 
 /**
  * Search deck given various parameters
@@ -20,17 +23,60 @@ var ObjectId = mongoose.Types.ObjectId;
 module.exports = async ({params, user = {}}, response, next) => {
     try {
         const limit = 10;
-        const searchtext = params.query;
-        const lang = user.preferedlocale || 'EN';
+        const lang = user.preferedlocale || 'EN'; 
 
-        let query = {
-            lang: lang,
-            'locale.EN.name': { "$regex": searchtext, "$options": "i" }
+        let searchtext = params.query;
+        let searchextras = {
+            power: undefined,
+            side: undefined,
+            release: undefined,
+            level: undefined,
+            cost: undefined,
+            colour: undefined,
+            cardtype: undefined,
         }
 
-        let cards = await Card.find(query).select(`_id locale.${lang}.name imagepath`).limit(limit).exec();
+        const hasquote = quoteexperession.exec(params.query)
 
-        console.log(query)              
+        if(!!hasquote){
+            let extras = searchtext.replace(hasquote[0], "").trim().split(' ')//retreave all text not inside the first matched quotes eg. ['blue', 'CX']
+            searchtext = hasquote[0].replace(/("|')/g, "")//store text in quotes, this will be either the name or ability search
+
+            extras.map((val) => {
+                if(cardtypes.includes(val.toUpperCase())){
+                    searchextras.cardtype = val.toUpperCase();
+                }
+                if(cardcolours.includes(val.toUpperCase())){
+                    searchextras.colour = val.toUpperCase();
+                }
+                if(val.match(/[0-9]\/[0-9]/)){// eg 1/0 represents level/cost. can test with %2F as the slash in url
+                    searchextras.level = val[0]
+                    searchextras.cost = val[2]
+                }
+                if(val.toUpperCase().match(/[S,W][0-9]+/)){ // eg S67, W87
+                    searchextras.side = val[0].toUpperCase()
+                    searchextras.release = val.substring(1)
+                }
+                if( Number.isInteger(Number(val)) && parseInt(val) >= 500 ){ //assume a number 500 or higher is the power
+                    searchextras.power = val
+                }
+            })
+        }   
+
+        //Delete undefined values to mongoose dosn't try to find them
+        Object.keys(searchextras).forEach(key => searchextras[key] === undefined ? delete searchextras[key] : {});
+
+        console.log(searchextras)
+
+        let query = {
+            ...searchextras,
+            $or: [
+                { [`locale.${lang}.name`]: { "$regex": searchtext, "$options": "i" } },
+                { [`locale.${lang}.ability`]: { "$regex": searchtext, "$options": "i" } },
+            ]            
+        }
+
+        let cards = await Card.find(query).select(`_id locale.${lang}.name imagepath`).limit(limit).exec();             
 
         response.status(200).json(cards)
     } catch (error) {
