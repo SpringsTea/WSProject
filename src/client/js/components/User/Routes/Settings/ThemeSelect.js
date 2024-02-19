9d2769f40bbb7de642d32a39983002195349ee1f
Shawn Springstead <shawn.springstead@gmail.com>
2024-02-19 15:12:09 -0500

    Theme select in user settings

---
+++ src/client/js/components/User/Routes/Settings/ThemeSelect.js
@@ -0,0 +1,32 @@
+import { Radio, Space, message } from 'antd';
+
+import { setUserConfig } from 'Utils/api';
+
+export default function ThemeSelect ({config = {}, onChange}){
+
+  const onSave = (e) => {
+
+    let radiovalue = e.target.value;
+
+    setUserConfig({'theme': radiovalue}).then((res) => {
+        if(res.success === true){
+          location.reload()
+        }
+        else{
+            message.error(res.message || 'Something went wrong')
+        }
+    })
+
+    
+  }
+
+  return(
+    <span>
+      Site Theme: <Space/>
+      <Radio.Group onChange={onSave} defaultValue={config.theme}>
+        <Radio value='light'> Light </Radio>
+        <Radio value='dark'> Dark </Radio>
+      </Radio.Group>
+    </span>
+  )
+};
