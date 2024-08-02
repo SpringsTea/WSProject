5787186fbdac90fd62b2484601d34787be25e4cc
Shawn Springstead <shawn.springstead@gmail.com>
2024-08-02 15:28:47 -0400

    Load card data from encore db

--- src/server/api/services/importdecklog.js
+++ src/server/api/services/importdecklog.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import Card from '../models/card'
+
 import GetDecklog from '../helpers/get-decklog';
 
 /**
@@ -17,7 +19,45 @@ module.exports = async (request, response, next) => {
         if( DecklogID ){
 
             const deckdata = await GetDecklog(DecklogID);
-            response.status(200).json(deckdata);
+
+            if( !deckdata.id ){
+                response.status(500).json({
+                    success: false,
+                    message: 'Decklog not found'
+                })
+                return false;
+            }
+
+            const decktitle = deckdata.title || 'New Deck';
+            let carderrors = [];
+
+            //Get the encore card object for each card in the deck data by it's card code
+            let cardpromises = deckdata.list.map((card) => {
+
+                //remove extra rarities from the card code
+                const cardcode = card?.card_number.replace(/-(\d+)([A-Z]*)$/, '-$1');
+
+                return Card.findOne({cardcode}).select('-ability -attributes -name -__v').exec()
+                .then((res) => {
+                    if(res){//will be null if not found
+                        return Array(card.num).fill(res)
+                    }
+                    else{
+                        carderrors.push(cardcode);
+                        return false;
+                    }
+                })
+            })
+
+            let cardlist = await Promise.all(cardpromises)
+            //remove false entries from the list (cards that wern't found)
+            cardlist = cardlist.filter((c) => c !== false);
+
+            response.status(200).json({
+                name: decktitle,
+                cards: cardlist,
+                carderrors,
+            });
         }
         else{
             response.status(500).json({
