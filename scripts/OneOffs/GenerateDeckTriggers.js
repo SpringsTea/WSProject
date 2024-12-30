c3e999b6056355a71dd47562a978a28bf7a1f15a
Shawn <shawn.springstead@gmail.com>
2024-12-30 17:12:10 -0500

    Move deck trigger calc to a helper

--- scripts/OneOffs/GenerateDeckTriggers.js
+++ scripts/OneOffs/GenerateDeckTriggers.js
@@ -7,6 +7,8 @@ const MODEL_PATH = '../../src/server/api/models';
 const DeckModel = require(`${MODEL_PATH}/deck`)
 const CardModel = require(`${MODEL_PATH}/card`)
 
+const DeckTriggers = require('../../src/server/api/helpers/deck-triggers')
+
 var mongooseOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -25,42 +27,14 @@ console.log('connected');
 
 try{
 	//triggers: { $exists: false }
-	let decks = DeckModel.find({ deleted: false, deckid:'4lDPaXFH_', valid: true }).batchSize(100).cursor();
-	console.log('Decks retreaved');
+	let decks = DeckModel.find({ deleted: false, triggers: { $exists: false }, valid: true }).batchSize(100).cursor();
+	console.log('Decks retrieved');
 
 	for await( const deck of decks ){
 
 		await deck.populate({ path: 'cards', match: { cardtype: 'CX' }, select: 'trigger' }).execPopulate();
 
-		let uniquecxs = [];
-		let decktriggers = [];
-
-		//deck.cards is only CX cards because of how the above populate works
-		deck.cards.map((card) => {
-
-			if(!card?.trigger || card?.trigger.length === 0){
-				return false;
-			}
-
-			if( uniquecxs.includes(card._id) ){
-				return false;
-			}
-			else{
-				uniquecxs.push(card._id)
-			}
-
-			//Special case for identifying double soul trigger
-			if( card?.trigger.length === 2 && card?.trigger?.[0] === 'SOUL' && card?.trigger?.[1] === 'SOUL'  ){
-				decktriggers.push('SOUL')
-				return true;
-			}
-
-			card?.trigger.map((trig) => {
-				if(trig !== 'SOUL'){
-					decktriggers.push(trig)
-				}			
-			})
-		})
+		let decktriggers = DeckTriggers.calcuateTriggers(deck);
 
 		deck.triggers = [...decktriggers];
 		await deck.save();
