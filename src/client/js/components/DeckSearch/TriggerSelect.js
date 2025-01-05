9e84d9982c95f32b2b21e4f6e4848eb4aba36355
Shawn Springstead <shawn.springstead@gmail.com>
2025-01-05 16:55:13 -0500

    Front end trigger select for deck searching

---
+++ src/client/js/components/DeckSearch/TriggerSelect.js
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { Select, Tag } from 'antd';
+import Img from 'react-image';
+
+import { 
+  QuestionCircleOutlined
+} from '@ant-design/icons';
+
+const { Option } = Select;
+
+export default function TriggerSelect({ onSelect }) {
+
+	const triggers = [
+		'SOUL',
+		'GATE',
+		'DRAW',
+		'CHOICE'
+	];
+
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+	const [selectedItems, setSelectedItems] = useState([]);
+
+	const handleSelect = (value) => {
+	  if( selectedItems.length === 0 || selectedItems.length >= 2 ){
+	  	setSelectedItems([value])
+	  }
+	  else{
+	  	setSelectedItems([selectedItems[0], value])
+	  	setDropdownOpen(false)
+	  }
+	};
+
+	const handleDeselect = (label) => {
+	    const updatedItems = selectedItems.filter((val) => val !== label);
+	    setSelectedItems(updatedItems);
+	};
+
+	const tagRender = (props) => {
+	  const { label, value, closable, onClose } = props;
+
+	  const trigger = label?.substring(1);
+
+	  const onPreventMouseDown = (event) => {
+	    event.preventDefault();
+	    event.stopPropagation();
+	  };
+	  return (
+	    <Tag
+	      onMouseDown={onPreventMouseDown}
+	      closable={closable}
+	      onClose={() => handleDeselect(label)}
+	      style={{
+	        marginInlineEnd: 4,
+	        display: 'flex'
+	      }}
+	    >
+	      <Img
+			style={{
+				width: '22px',
+			}}
+		    src={`/images/assets/triggers/${trigger}.png`}
+	    	unloader={<QuestionCircleOutlined className="image-not-found" />}
+	    />
+	    </Tag>
+	  );
+	};
+
+	return (
+		<div>
+			<Select
+		      mode="multiple"
+		      value={selectedItems}
+		      open={dropdownOpen}
+		      onDropdownVisibleChange={(open) => setDropdownOpen(open)} // Syncs manual and user interaction
+		      style={{ width: "100%" }}
+		      tagRender={tagRender}
+		      dropdownRender={(menu) => (
+		      	<div>
+		      		<div>
+		      			{
+		      				triggers.map((trigger) => (
+		      					<Img
+		      						className="clickable"
+		      						onClick={() => handleSelect(`${selectedItems.length}${trigger}`)}
+									style={{
+										width: '32px',
+										marginRight: '8px'
+									}}
+								    src={`/images/assets/triggers/${trigger}.png`}
+							    	unloader={<QuestionCircleOutlined className="image-not-found" />}
+							    />
+		      				))
+		      			}
+		      		</div>
+		      	</div>
+		      )}
+		    >
+
+		    </Select>
+		</div>
+
+	)
+}
