57e2fb86b080dacedf3731a8723e1e2278a72e2f
Shawn Springstead <stang90@gmail.com>
2026-05-24 15:45:56 -0400

    Image download and series creation completed

--- scripts/JPRESTWeissImport/index.js
+++ scripts/JPRESTWeissImport/index.js
@@ -2,13 +2,20 @@ const path = require('path');
 
 const { getExpansions } = require('./Expansions');
 const { getCards } = require('./Cards');
-const { getSeries } = require('./Mongo/Series');
+const Scrape = require('./ImageScrape/ScrapeV3.js'); 
+const { getSeries, createSeries } = require('./Mongo/Series');
 const { getCard, createCard } = require('./Mongo/Card');
 
 const mongoose = require('mongoose')
 var ObjectId = mongoose.Types.ObjectId;
 
 let EXPANSION_ID =  process.env.EXPANSION_ID || null;
+
+const ImageUrl = 'https://ws-tcg.com/wordpress/wp-content/images/cardlist/';
+const IMAGE_DESTINATION = path.resolve(
+    __dirname,
+    '../../public/images/JP'
+)
 
 const config = require('../../src/server/config/mongo.js')
 var mongooseOptions = {
@@ -25,6 +32,7 @@ mongoose.connect(`mongodb://127.0.0.1:27017/wsdata?authSource=admin`, mongooseOp
 console.log('connected');
 
 async function Go(){
+	let CardImages = [];
 	const ExpansionList = await getExpansions({ id: EXPANSION_ID }) //Keep in mind that TDs have their own seperate expansions
 	console.log(`${ExpansionList.length} expansions found`)
 	for (const expansion of ExpansionList) {
@@ -38,11 +46,37 @@ async function Go(){
 	  	let series = null;
 	  	if( !serieses.get(`${card.side}-${card.release}`)){
 	  		series = await getSeries({ side: card.side, release: card.release })
+
+	  		//No series is found, lets create it
+	  		if( !series ){
+	  			series = await createSeries({
+	  				set: card.set,
+	  				side: card.side,
+	  				release: card.release,
+	  				lang: 'JP',
+	  				hash: new ObjectId(),
+	  				expansions: [ card.expansion ],
+	  				update_date: expansion.update_date,
+	  			})
+	  		}
+
 	  		serieses.set(`${series.side}-${series.release}`, series)
 	  	}
 	  	else{
 	  		series = serieses.get(`${card.side}-${card.release}`)
 	  	}
+
+	  	//If series already exists but dosn't include this cards expansion
+			if( !series.expansions.includes( card.expansion ) ){
+				series.expansions = [...series.expansions, card.expansion]
+				series.save();
+			}
+
+	  	CardImages.push({
+	  		...card, 
+	  		remoteimage: `${ImageUrl}${card.imagepath}`,
+	  		imagedestination: `${IMAGE_DESTINATION}/${card.set}/${card.side}${card.release}`,
+	  	})
 
 	  	let remotecard = await getCard({ cardcode: card.cardcode })
 	  	
@@ -50,35 +84,37 @@ async function Go(){
 
 	  		const imgext = path.extname(card.imagepath)
 
-			remotecard.locale['JP'] = {
-				name: card.name,
-				ability: card.ability || [],
-				attributes: card.attributes || [],
-			}
+				remotecard.locale['JP'] = {
+					name: card.name,
+					ability: card.ability || [],
+					attributes: card.attributes || [],
+				}
 
-			remotecard.level = card.level
-			remotecard.colour = card.colour
-			remotecard.cardtype = card.cardType 
-			remotecard.imagepath = `JP/${card.set}/${card.side}${card.release}/${card.sid}${imgext}`
-			remotecard.armycount = card.armylimit;
+				remotecard.level = card.level
+				remotecard.colour = card.colour
+				remotecard.cardtype = card.cardtype 
+				remotecard.imagepath = `JP/${card.set}/${card.side}${card.release}/${card.sid}${imgext}`
+				remotecard.armycount = card.armylimit;
 
-			//Only update series if there is none set
-			//Never override existing series because sometime is has to be manually set because fuck bushi
-			if( remotecard.series == null ){
-				remotecard.series = series ? series._id : null;
-			}
+				//Only update series if there is none set
+				//Never override existing series because sometime is has to be manually set because fuck bushi
+				if( remotecard.series == null ){
+					remotecard.series = series ? series._id : null;
+				}
 
-			await remotecard.save();
-			console.log('Card Saved', remotecard.cardcode, remotecard._id);
+				await remotecard.save();
+				console.log('Card Saved', remotecard.cardcode, remotecard._id);
 	  	}
 	  	else{
-			await createCard(card, series)
+				await createCard(card, series)
 	  	}
 	  }
 
 	  let remoteseries = await getSeries({ expansion: expansion.id })
 	  remoteseries.update_date = new Date()
 	  remoteseries.save();
+
+	  await Scrape(CardImages)
 	}
 	
 }
